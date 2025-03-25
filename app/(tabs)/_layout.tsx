655f212fcff3e8048d4f7e9f0a24e236dd0dee0f
danhdev98 <danh.vo.2110@gmail.com>
2025-03-25 17:00:04 +0700

    create project

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+import MaterialIcon from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { COLORS } from '@/constants/themes';
+
+export default function Tablayout() {
+
+    return (
+        <Tabs screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarInactiveTintColor: COLORS.white,
+            tabBarStyle: {
+                backgroundColor: "black",
+                borderTopWidth: 0,
+                height: 40,
+                elevation: 0,
+                position: "absolute",
+                paddingBottom: 8
+            }
+        }}>
+            <Tabs.Screen name="index" options={{ tabBarIcon: ({ size, color }) => <Ionicons name='home' size={size} color={color} /> }} />
+            <Tabs.Screen name="bookmarks" options={{ tabBarIcon: ({ size, color }) => <FontAwesome name='bookmark' size={size} color={color} /> }} />
+            <Tabs.Screen name="create" options={{ tabBarIcon: ({ size, color }) => <Ionicons name='create' size={size} color={COLORS.primary} /> }} />
+            <Tabs.Screen name="notifications" options={{ tabBarIcon: ({ size, color }) => <MaterialIcon name='bell-ring' size={size} color={color} /> }} />
+            <Tabs.Screen name="profile" options={{ tabBarIcon: ({ size, color }) => <FontAwesome name='user' size={size} color={color} /> }} />
+        </Tabs>
+
+    )
+}
+
---
+++ constants/themes.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    primary: "green",
+    secondary: "blue",
+    background: "white",
+    surface: "#1A1A1A",
+    sucfaceLight: "#2A2A2A",
+    white: "white",
+    gray: "gray",
+} as const
